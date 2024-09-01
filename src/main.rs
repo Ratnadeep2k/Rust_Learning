1013245f1373ff4fc3593f2e77453a8216d24843
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-01 21:25:12 +0530

    refactor: Declare variable with explicit type annotation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut x =4; //i:32  is 32 b it integer 
+    let mut x:i32 =4; //i:32  is 32 b it integer 
     println!("Hello, world! is {}",x);
     x =3;
     println!("Reassign Value using mut {}",x);
