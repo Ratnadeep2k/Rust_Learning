ffc54c34e8297ac3bf06d1d7b41afe70454908fa
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-02 19:34:37 +0530

    refactor: Use tuple destructuring to improve variable assignment

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,13 @@ fn main() {
      println!("The first value is {}",a.0);
      println!("The second value is {}",a.1);
      println!("The third value is {}",a.2);
-     
+
+
+     let b = (1,2,3);
+
+     //option 1
+     let val_one= b.0;
+     let val_two= b.1;
+     println!("The first value is {},and second value is {}",val_one,val_two);
+
 }
