79515252de3f55155d2d29d97cd8b90b79a01e9a
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-02 19:32:35 +0530

    Refactor code to use tuples for variable assignment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x:i32 =4; //i:32  is 32 b it integer 
-    println!("Hello, world! is {}",x);
-    x =3;
-    println!("Reassign Value using mut {}",x);
+    // let mut x:i32 =4; //i:32  is 32 bit integer 
+    // println!("Hello, world! is {}",x);
+    // x =3;
+    // println!("Reassign Value using mut {}",x);
+     //Tuples 
+
+     let a = (5,"Hello world",false);
+     println!("The first value is {}",a.0);
+     println!("The second value is {}",a.1);
+     println!("The third value is {}",a.2);
+     
 }
